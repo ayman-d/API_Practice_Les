1a07b4430405f2aba8ef286840020d304ff9584b
ayman-d <aymand1982@gmail.com>
2021-01-24 15:36:44 -0700

    Fix slight reference error in CommandReadDTO.cs

--- DTOs/CommandDTOs/CommandReadDTO.cs
+++ DTOs/CommandDTOs/CommandReadDTO.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Commander.DTOs.FeedbackDTOs;
-using Commander.Models;
 
 namespace Commander.DTOs.CommandDTOs
 {
